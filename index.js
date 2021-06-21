c98fe633b274e0f4390916b3d90ad62d4c212688
Alexander <akrichevcov96@mail.ru>
2021-06-21 15:00:32 +0500

    Drag-n-drop has been created.

---
+++ index.js
@@ -0,0 +1,41 @@
+const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
+
+const dragstart = (event) => {
+    event.target.classList.add('hold');
+    setTimeout(() => {
+        event.target.classList.add('hide');
+    }, 0)
+}
+
+const dragend = (event) => {
+    event.target.classList.remove('hold');
+    event.target.classList.remove('hide');
+}
+
+const dragover = (event) => {
+    event.preventDefault();
+}
+
+const dragenter = (event) => {
+    event.target.classList.add('hovered');
+}
+
+const dragleave = (event) => {
+    event.target.classList.remove('hovered');
+}
+
+const dragdrop = (event) => {
+    event.target.classList.remove('hovered');
+    event.target.append(item);
+}
+
+item.addEventListener('dragstart', dragstart);
+item.addEventListener('dragend', dragend);
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover);
+    placeholder.addEventListener('dragenter', dragenter);
+    placeholder.addEventListener('dragleave', dragleave);
+    placeholder.addEventListener('drop', dragdrop);
+}
